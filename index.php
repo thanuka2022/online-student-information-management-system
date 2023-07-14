62a723c0cb3425691d2289f0255861339cd7ddda
thanuka2022 <113705892+thanuka2022@users.noreply.github.com>
2023-07-14 17:29:47 +0530

    change login page

--- index.php
+++ index.php
@@ -8,30 +8,44 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    // Begin the transaction
-    $conn->begin_transaction();
+    // Perform basic validation
+    $errors = array();
 
-    try {
-        // Check if the username and password exist in the database
-        $checkQuery = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
-        $result = $conn->query($checkQuery);
+    if (empty($username)) {
+        $errors[] = "Username is required.";
+    }
 
-        if ($result && $result->num_rows > 0) {
-            // Valid username and password, set the user session and commit the transaction
-            $_SESSION['username'] = $username;
-            $conn->commit();
-            header('Location: dashboard.php');
-            exit();
-        } else {
-            // Invalid username or password, rollback the transaction and show an error message
+    if (empty($password)) {
+        $errors[] = "Password is required.";
+    }
+
+    // Proceed with login if there are no validation errors
+    if (empty($errors)) {
+        // Begin the transaction
+        $conn->begin_transaction();
+
+        try {
+            // Check if the username and password exist in the database
+            $checkQuery = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+            $result = $conn->query($checkQuery);
+
+            if ($result && $result->num_rows > 0) {
+                // Valid username and password, set the user session and commit the transaction
+                $_SESSION['username'] = $username;
+                $conn->commit();
+                header('Location: dashboard.php');
+                exit();
+            } else {
+                // Invalid username or password, rollback the transaction and show an error message
+                $conn->rollback();
+                $errors[] = "Invalid username or password.";
+            }
+        } catch (Exception $e) {
+            // Error occurred, rollback the transaction and handle the error
             $conn->rollback();
-            $error = "Invalid username or password.";
+            $errors[] = "An error occurred during the login process.";
+            // You can also log the error to a log file or perform additional error handling here
         }
-    } catch (Exception $e) {
-        // Error occurred, rollback the transaction and handle the error
-        $conn->rollback();
-        $error = "An error occurred during the login process.";
-        // You can also log the error to a log file or perform additional error handling here
     }
 
     // Close the database connection
@@ -56,8 +70,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <center><img src="images/logo.png" class="logo"></center>
         <h2>Welcome</h2>
         <form action="" method="post">
-            <?php if (isset($error)) { ?>
-                <div class="error"><?php echo $error; ?></div>
+            <?php if (!empty($errors)) { ?>
+                <div class="error">
+                    <ul>
+                        <?php foreach ($errors as $error) { ?>
+                            <li><?php echo $error; ?></li>
+                        <?php } ?>
+                    </ul>
+                </div>
             <?php } ?>
             <div class="user-box">
                 <input type="text" name="username" required="">
@@ -79,9 +99,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             </div>
             <input type="submit" class="submit-button" value="Login">
         </form>
-        <p class="forgot-password-link">
-            <a href="forgotpassword.php">I forgot my password</a>
-        </p>
+        <div class="row">
+            <p class="text-center text-muted mt-3 mb-0">Not Registered?
+                <a href="reg.php" style="color: #403e3e;"><u>Create an account</u></a>
+                <a id="btn" class="text-danger " href="forgotpassword.php" style="font-size: 15px;">I forgot my password</a>
+            </p>
+        </div>
     </div>
 </body>
 </html>
